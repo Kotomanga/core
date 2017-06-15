432bb91a43040f1fbc8d724b62a1bbc0788a26d4
Kyler Chin <kylerschin@gmail.com>
2017-06-15 04:09:03 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-//const bitcoin = require('./src/bitcoin');
 const ledger = require('./src/ledger');
 const peer = require('./src/peering');
 const power = require('./src/power');
