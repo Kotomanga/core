dadccaa61ae97ba77f50dea2cfc66b07a7602ae4
Kyler Chin <kylerschin@users.noreply.github.com>
2017-07-23 20:01:18 -0700

    Update solarcredits.js

--- src/token/solarcredits.js
+++ src/token/solarcredits.js
@@ -1,4 +1,15 @@
-contract MyToken {
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
+contract solarCredits {
+	/* This creates an array with all balances */
+	mapping (address => uint256) public balanceOf;
+
+	function solarCredits() {
+		balanceOf[msg.sender] = 21000000;
+	}
+	
+	/* Send coins */
+	function transfer(address _to, uint256 _value) {
+	    /* Add and subtract new balances */
+	    balanceOf[msg.sender] -= _value;
+	    balanceOf[_to] += _value;
+	}
 }
