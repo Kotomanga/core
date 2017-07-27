138822dc922da431ade33e7e10a10bc50ff99e9e
Kyler Chin <kylerschin@gmail.com>
2017-07-27 16:28:41 -0700

    Fix async dependncy bug IPFS and Link to Ether file
    Added async, ipfs, ipfs-api dependencies.

--- src/ipfs/deploy.js
+++ src/ipfs/deploy.js
@@ -1,5 +1,11 @@
 const IPFS = require('ipfs')
 const node = new IPFS()
+const async = require("async")
+async.map(
+    ['index.js','package.json','npm-shrinkwrap.json'],
+    require("fs").stat,
+    (err, results) => console.log(results)
+);
 
 node.on('ready', () => {
   // Your node is now ready to use \o/
@@ -10,39 +16,33 @@ node.on('ready', () => {
   })
 })
 
-series([
-  (cb) => node.on('ready', cb),
-  (cb) => node.version((err, version) => {
-    if (err) { return cb(err) }
-    console.log('Version:', version.version)
-    cb()
-  })
-])
-
+//Function pass input and return hash
 var deployIpfs = function(input) {
-  //Add string "input" to node
+  series([
+    (cb) => node.on('ready', cb),
+    (cb) => node.version((err, version) => {
+      if (err) { return cb(err) }
+      console.log('Version:', version.version)
+      ipfsVersion = 'Version:', version.version;
+      cb()
+    }),
     (cb) => node.files.add({
       content: Buffer.from(input)
     }, (err, result) => {
       if (err) { return cb(err) }
 
-      // Once the file is added, we get back an object containing the path, the
-      // multihash and the sie of the file
       console.log('\nAdded file:', result[0].path, result[0].hash)
       fileMultihash = result[0].hash
-      cb()
-})
-//Return hash value to function
-//This will be passed back into the blockchain
-return(result[0].hash);
+      //return hash to function
+      return(result[0].hash);
+      // cb()
+    }),
+    (cb) => node.files.cat(fileMultihash, (err, stream) => {
+      if (err) { return cb(err) }
 
-//read the string back
-(cb) => node.files.cat(fileMultihash, (err, stream) => {
-  if (err) { return cb(err) }
-
-  console.log('\nFile content:')
-  // print the file to the terminal and then exit the program
-  stream.pipe(process.stdout)
-  stream.on('end', process.exit)
-})
+      console.log('\nFile content:')
+      stream.pipe(process.stdout)
+      stream.on('end', process.exit)
+    })
+  ])
 }
