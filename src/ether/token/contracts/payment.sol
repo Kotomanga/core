c7188fd059c344f324db3604021aaba2ad963d50
Kyler Chin <kylerschin@gmail.com>
2017-08-10 20:05:45 -0700

    create vars needed for cycle in transactions

--- src/ether/token/contracts/payment.sol
+++ src/ether/token/contracts/payment.sol
@@ -1,12 +1,23 @@
 pragma solidity ^0.4.13;
 
 contract payment {
+  struct TxStats { // Struct
+        uint cycleTime;
+        address _p;
+        address _c;
+        string _PId;
+        string _CId;
+    }
 
   function createSlowAllowance() {
 
   }
 
-  function runCycle() {
-    
-  }
+  function runCycle(
+        uint _Pe,
+        uint _Ce,
+    ) {
+        producer = _Pe;
+        consumer = _ce;
+    }
 }
