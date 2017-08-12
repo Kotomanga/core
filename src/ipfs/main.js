aa433fa52c7908972a53f000995fd8d4f12311af
Kyler Chin <kylerschin@gmail.com>
2017-08-12 16:08:09 -0700

    Finally added ipfs.files.add

--- src/ipfs/main.js
+++ src/ipfs/main.js
@@ -1,5 +1,13 @@
-function deploy (data) {
+const IPFS = require('ipfs')
+const node = new IPFS()
 
+function deploy (data) {
+  ipfs.files.add(
+    {
+      path: '',
+      content: "content"
+    },
+     [options], [callback])
 }
 
 module.exports.deploy = deploy;
