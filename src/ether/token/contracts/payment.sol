a25f6399e7316ccc471aee7a4a597d03835cbde3
Kyler Chin <kylerschin@gmail.com>
2017-08-15 11:40:38 -0700

    Add some credits to solidity contract

--- src/ether/token/contracts/payment.sol
+++ src/ether/token/contracts/payment.sol
@@ -1,3 +1,17 @@
+//sol
+// Multi-party, anti-fraud, partial-wallet for buying and selling electricity.
+// @authors:
+// Kyler Chin , <kylerschin@gmail.com>
+//
+// usage:
+// SolCredits can be deposited into this contract and only sent with each cycle by event approval.
+// Funds can be refunded and withdrawn.
+// P pays for all Tx.
+// Cycle times chosen by P.
+// Trust is uniform
+// Thus, Both parties are protected from fraud.
+// Get a free potato! Have a great day in a great life!
+
 pragma solidity ^0.4.13;
 
 contract payment {
@@ -11,7 +25,12 @@ contract payment {
         event Revoke(address owner, bytes32 operation);
 
 //FIGURE OUT WAY TO DISTRIBUTE TRANSACTION WITH Pe, Ce, msolPerWatt and not have one person controlling all the dials.
-  function runCycle(
+  function runCycle()
 
+    }
+
+    function withdraw() {
+        if (msg.sender.send(shares[msg.sender]))
+            shares[msg.sender] = 0;
     }
 }
